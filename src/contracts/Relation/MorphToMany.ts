de7bc86c55683bdae251270bd05354e5dffe2b15
Bruno Polo <brunopolo@poli.ufrj.br>
2026-09-01 16:31:33 -0300

    fix(relations): remove filtros de pivot em MorphToMany.query()
    - Herda `BelongsToMany.query()` sem acréscimo: o filtro pela relação inversa
      já restringe o pivot no backend
    - `where[{morph}_id]` e `where[{morph}_type]` são colunas do pivot, não
      atributos do model relacionado, e o `ModelFilter` do backend respondia 500
    - Corrige o teste que fixava o comportamento antigo e cobre também o lado
      `morphedByMany`
    
    Resolve #1

--- src/contracts/Relation/MorphToMany.ts
+++ src/contracts/Relation/MorphToMany.ts
@@ -1,29 +1,6 @@
-import { BuilderInterface as Builder } from '../../types/Builder';
-import { Model, ModelPaginatedResponse } from '../../types/Model';
-
 import BelongsToMany from './BelongsToMany';
 
 
-type BuilderInterface = Builder<Model, ModelPaginatedResponse>;
-
 export default class MorphToMany extends BelongsToMany
 {
-
-    query(): BuilderInterface {
-        const query = super.query();
-
-        // const relationName = this.guessInverseRelation();
-        // const relation = this.getRelated().getSchema().relations
-        const morphId = `${this.meta.morphType?.slice(0, -5)}_id`;
-
-        query.where(morphId, this.parent.getKey());
-        query.where(this.meta.morphType!, this.parent.getType());
-        query.lock(`where.${morphId}`);
-        query.lock(`where.${this.meta.morphType}`);
-
-        return query;
-    }
-
 }
-
-
